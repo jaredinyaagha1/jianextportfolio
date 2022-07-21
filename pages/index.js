fdabf8f4ae7aadf10c20afb372849b650bb9fc42
jaredinyaagha1 <jaredinyaagha@gmail.com>
2022-07-21 15:38:37 -0400

    can I get some rest?

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Can I please get <a href="https://nextjs.org">some rest?</a>
         </h1>
 
         <p className={styles.description}>
